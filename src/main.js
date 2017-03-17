d1ceb5a328e0cc81519c90974d0552afc88a842d
Michael Arnold <michael.arnold@lifeway.com>
2017-03-17 11:10:39 -0500

    Fully working client-side code.

--- src/main.js
+++ src/main.js
@@ -8,12 +8,27 @@ if (!window.Promise) {
 
 import 'whatwg-fetch';
 
-function queryStats() {
+function getParams() {
   const params = {
-      site: 'www.marmanold.com', 
-      start: '2017-03-15', 
-      end: '2017-04-01'
+      site: document.getElementById('site').value, 
+      start: document.getElementById('start').value, 
+      end: document.getElementById('end').value
   };
+
+  return params;
+}
+
+function clearStats() {
+  let responseRoot = document.getElementById('results');
+
+  //Clear Elements from the list before adding again
+  while(responseRoot.firstChild) {
+    responseRoot.removeChild(responseRoot.firstChild);
+  }
+}
+
+function queryStats() {
+  const params = getParams();
 
   fetch('https://bz2gq8s1sb.execute-api.us-east-1.amazonaws.com/dev/stats', {
     method: 'POST',
@@ -28,13 +43,22 @@ function queryStats() {
     }).then(function(json) {
       let responseRoot = document.getElementById('results');
 
+      //Clear Elements from the list before adding again
+      clearStats();
+
+      //Add each visit to the list
       json.forEach(function(visit) {
         let newVisit = document.createElement('li');
         newVisit.appendChild(document.createTextNode(JSON.stringify(visit)));
         responseRoot.appendChild(newVisit);
-        console.log(JSON.stringify(visit));
       });
     }).catch(function(ex) {
       console.log('parsing failed', ex)
     });
+}
+
+//Wait for the page to finish loading before starting to watch for a click
+window.onload = function() {
+  document.getElementById('query').addEventListener('click', queryStats, false);
+  document.getElementById('clear').addEventListener('click', clearStats, false);
 }
