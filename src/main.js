3539db033eb66677677a6ab0e093d41f9ec39be6
marmanold <michael@rnold.info>
2017-03-17 09:35:52 -0500

    Refactoring to attempt to make it more userfriendly

--- src/main.js
+++ src/main.js
@@ -8,33 +8,33 @@ if (!window.Promise) {
 
 import 'whatwg-fetch';
 
-const params = {
-    site: 'www.marmanold.com', 
-    start: '2017-03-15', 
-    end: '2017-04-01'
-};
+function queryStats() {
+  const params = {
+      site: 'www.marmanold.com', 
+      start: '2017-03-15', 
+      end: '2017-04-01'
+  };
 
-fetch('https://bz2gq8s1sb.execute-api.us-east-1.amazonaws.com/dev/stats', {
-  method: 'POST',
-  headers: {
-    'Accept': 'application/json, application/xml, text/plain, text/html, *.*',
-    'Content-Type': 'application/json'
-  },
-  mode: 'cors',
-  body: JSON.stringify(params)
-}).then(function(response) {
-    return response.json()
-  }).then(function(json) {
-    let responseRoot = document.getElementById('results');
+  fetch('https://bz2gq8s1sb.execute-api.us-east-1.amazonaws.com/dev/stats', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json, application/xml, text/plain, text/html, *.*',
+      'Content-Type': 'application/json'
+    },
+    mode: 'cors',
+    body: JSON.stringify(params)
+  }).then(function(response) {
+      return response.json()
+    }).then(function(json) {
+      let responseRoot = document.getElementById('results');
 
-    json.forEach(function(visit) {
-    	let newVisit = document.createElement('li');
-    	newVisit.appendChild(document.createTextNode(JSON.stringify(visit)));
-    	responseRoot.appendChild(newVisit);
-    	console.log(JSON.stringify(visit));
+      json.forEach(function(visit) {
+        let newVisit = document.createElement('li');
+        newVisit.appendChild(document.createTextNode(JSON.stringify(visit)));
+        responseRoot.appendChild(newVisit);
+        console.log(JSON.stringify(visit));
+      });
+    }).catch(function(ex) {
+      console.log('parsing failed', ex)
     });
-  }).catch(function(ex) {
-    console.log('parsing failed', ex)
-  });
-
-
+}
