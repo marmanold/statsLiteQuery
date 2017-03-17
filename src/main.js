61ec5ffe8d3374e24bcb3037e173b7140013bb13
marmanold <michael@rnold.info>
2017-03-16 21:22:43 -0500

    Finally have it working!

--- src/main.js
+++ src/main.js
@@ -9,7 +9,9 @@ if (!window.Promise) {
 import 'whatwg-fetch';
 
 const params = {
-    site: 'www.marmanold.com'
+    site: 'www.marmanold.com', 
+    start: '2017-03-15', 
+    end: '2017-04-01'
 };
 
 fetch('https://bz2gq8s1sb.execute-api.us-east-1.amazonaws.com/dev/stats', {
@@ -18,8 +20,21 @@ fetch('https://bz2gq8s1sb.execute-api.us-east-1.amazonaws.com/dev/stats', {
     'Accept': 'application/json, application/xml, text/plain, text/html, *.*',
     'Content-Type': 'application/json'
   },
-  mode: 'no-cors',
+  mode: 'cors',
   body: JSON.stringify(params)
-}).then(function(response) {return response.text();}).then(function(text){console.log(`Text: ${text}`);});
+}).then(function(response) {
+    return response.json()
+  }).then(function(json) {
+    let responseRoot = document.getElementById('results');
+
+    json.forEach(function(visit) {
+    	let newVisit = document.createElement('li');
+    	newVisit.appendChild(document.createTextNode(JSON.stringify(visit)));
+    	responseRoot.appendChild(newVisit);
+    	console.log(JSON.stringify(visit));
+    });
+  }).catch(function(ex) {
+    console.log('parsing failed', ex)
+  });
 
 
